2d1e8225854c3d08a595ec5c9fd006478a8d6bd5
Charlie <charlie@owlsworth.co.za>
2024-09-07 14:47:41 +0200

    fixed bug in api... (just getting started)

--- peach_api.py
+++ peach_api.py
@@ -1,38 +1,44 @@
-# peach_api.py
-
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+# Initialize Tamagotchi instance globally
+tamagotchi_instance = None  # This will hold the reference to the Tamagotchi instance
+
 
 class TamagotchiAPI:
-	def __init__(self, tamagotchi_instance):
-		self.tamagotchi = tamagotchi_instance
+	def __init__(self, tamagotchi):
+		global tamagotchi_instance
+		tamagotchi_instance = tamagotchi  # Assign the Tamagotchi instance to global
 
 	def start_api(self):
-		# Start the Flask server (could be threaded later)
 		app.run(host='0.0.0.0', port=5000)
 
 
 @app.route('/status', methods=['GET'])
 def get_status():
-	# Return Peach's current stats
-	status = {'name': self.tamagotchi.name, 'health': self.tamagotchi.health, 'hunger': self.tamagotchi.hunger, 'happiness': self.tamagotchi.happiness, 'hygiene': self.tamagotchi.hygiene, 'age': self.tamagotchi.age, 'sleeping': self.tamagotchi.sleeping}
-	return jsonify(status)
+	global tamagotchi_instance
+	if tamagotchi_instance:
+		status = {'name': tamagotchi_instance.name, 'health': tamagotchi_instance.health, 'hunger': tamagotchi_instance.hunger, 'happiness': tamagotchi_instance.happiness, 'hygiene': tamagotchi_instance.hygiene, 'age': tamagotchi_instance.age, 'sleeping': tamagotchi_instance.sleeping}
+		return jsonify(status)
+	else:
+		return jsonify({"error": "Tamagotchi instance not found"}), 500
 
 
 @app.route('/action', methods=['POST'])
 def perform_action():
+	global tamagotchi_instance
 	data = request.json
 	action = data.get('action', None)
-	if action:
+
+	if tamagotchi_instance and action:
 		if action == 'feed':
-			self.tamagotchi.feed()
+			tamagotchi_instance.feed()
 		elif action == 'clean':
-			self.tamagotchi.clean()
+			tamagotchi_instance.clean()
 		elif action == 'play':
-			self.tamagotchi.play()
+			tamagotchi_instance.play()
 		elif action == 'medicine':  # Added medicine action
-			self.tamagotchi.give_medicine()
+			tamagotchi_instance.give_medicine()
 		return jsonify({'message': f'Action {action} performed'})
 	return jsonify({'message': 'Invalid action'}), 400
